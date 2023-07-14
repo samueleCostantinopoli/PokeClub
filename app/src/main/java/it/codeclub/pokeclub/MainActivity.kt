bcd8044b755b7490bfa3f2c82a089d60a2dc8e6f
Maurizioore <mauriziorenzi68@gmail.com>
2023-07-14 16:39:56 +0200

    avvio app

--- app/src/main/java/it/codeclub/pokeclub/MainActivity.kt
+++ app/src/main/java/it/codeclub/pokeclub/MainActivity.kt
@@ -1,24 +1,24 @@
 package it.codeclub.pokeclub
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import it.codeclub.pokeclub.remote.data.PokemonList
-import it.codeclub.pokeclub.ui.theme.PokeClubTheme
-
+import dagger.hilt.android.AndroidEntryPoint
+import it.codeclub.pokeclub.db.PokemonDao_Impl
+import it.codeclub.pokeclub.db.PokemonDatabase_Impl
+import it.codeclub.pokeclub.db.PokemonRepositoryDbImpl
+import it.codeclub.pokeclub.pokemonlist.PokedexScreen
+import it.codeclub.pokeclub.pokemonlist.PokemonListViewModel
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val intent = Intent(this, PokemonList::class.java)
-        startActivity(intent)
+        //viene lanciata una nuova activity
+        setContent {
+            val database=PokemonDatabase_Impl()
+            val pokemonRepositoryDbImpl:PokemonRepositoryDbImpl= PokemonRepositoryDbImpl(PokemonDao_Impl(database))
+            val pokemonListViewModel:PokemonListViewModel=PokemonListViewModel(pokemonRepositoryDbImpl)
+            PokedexScreen(this, pokemonListViewModel)
+        }
     }
 }
